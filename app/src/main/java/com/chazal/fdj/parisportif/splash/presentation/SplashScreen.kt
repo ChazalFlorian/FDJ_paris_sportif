54389a690055268637b188d66059a297bcedcfe1
ChazalFlorian <chazal.florian@gmail.com>
2025-09-01 07:26:28 +0200

    fix: splash design

--- app/src/main/java/com/chazal/fdj/parisportif/splash/presentation/SplashScreen.kt
+++ app/src/main/java/com/chazal/fdj/parisportif/splash/presentation/SplashScreen.kt
@@ -1,22 +1,33 @@
 package com.chazal.fdj.parisportif.splash.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.chazal.fdj.parisportif.R
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
-    Column {
+fun SplashScreen(
+    goToNext: () -> Unit,
+) {
+    LaunchedEffect(key1 = Unit) {
+        delay(500)
+        goToNext()
+    }
+    Column(
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = null,
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
+                .align(Alignment.CenterHorizontally),
         )
     }
 }
@@ -24,5 +35,5 @@ fun SplashScreen() {
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    SplashScreen()
+    SplashScreen(goToNext = {})
 }
